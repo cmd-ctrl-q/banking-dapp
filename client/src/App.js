c27db3c5e0bbee082da5b171f09c932304c24a0b
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-04-01 15:20:37 -0500

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+
+// import artifacts
+
+function App() {
+  return <div className='App'>working...</div>;
+}
+
+export default App;
